5d8b8ac3d34d946eeae07308d4f474b6868a530b
BS1328 <faiza.mushtari@brainstation-23.com>
2023-04-19 05:24:21 +0600

    Show H2 Database Console

--- build/generated/sources/annotationProcessor/java/main/com/brainstation23/erp/mapper/UserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/brainstation23/erp/mapper/UserMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-17T17:27:07+0600",
+    date = "2023-04-19T05:22:23+0600",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 17.0.6 (Amazon.com Inc.)"
 )
 @Component
